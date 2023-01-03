138696a105843f5cb16cbe04ca89ac79b90cac35
askPavan <pavankumarambraji@gmai.com>
2023-01-03 10:03:35 +0530

    instance preferIpAddress added

--- src/main/java/com/ms/eclient/ProductController.java
+++ src/main/java/com/ms/eclient/ProductController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	@GetMapping("/products/{prodNo}")
-	public String home(@PathVariable("prodNo") int prodNo) {
+	public String home(@PathVariable("prodNo") int prodNo) throws InterruptedException {
 		System.out.println("Fetching Product Details....."+prodNo);
+		//TODO: uncomment thread.sleep() when you want to forcefully down the service for testing circuit breaker
+		// circuit breaker enabled in product-service client application
+		//Thread.sleep(1000*5);
+		System.out.println("successfully Fetched product details, for "+prodNo);
 		return "Feching Product details for: "+prodNo;
 	}
 }
